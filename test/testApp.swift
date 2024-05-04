70473a5e7c5ba6f610dad8daf8ecddabb6e26fca
bxb1pro <bxb1pro@gmail.com>
2024-05-04 13:08:22 +0100

    test

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Brad on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
